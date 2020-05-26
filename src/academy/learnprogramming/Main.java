4597acf2abaefca4b87cd69413ac139e2169c1bf
ryoray <praretno.wibowo@gmail.com>
2020-05-26 18:05:06 +0700

    update 05/20 - add polymorphism methods

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -20,25 +20,27 @@ class Car {
     }
 
     public int accelerate(int speed) {
+        int velocity = this.getVelocity();
         if (speed < 0) {
-            return this.velocity;
+            System.out.println("The car is moving constantly at " + velocity);
         } else {
             this.velocity += speed;
+            System.out.println("The car's velocity was added by " +
+                    speed + " and now become " + this.velocity);
         }
-        System.out.println("The car's velocity was added by " +
-                speed + " and now become " + this.velocity);
-        return this.velocity;
+        return velocity;
     }
 
     public int brake (int speed) {
+        int velocity = this.getVelocity();
         if (speed < 0) {
-            return this.velocity;
+            System.out.println("The car speed is moving constantly at " + velocity);
         } else {
             this.velocity -= speed;
+            System.out.println("The car's velocity was reduced by " +
+                    speed + " and now become " + velocity);
         }
-        System.out.println("The car's velocity was reduced by " +
-                speed + " and now become " + this.velocity);
-        return this.velocity;
+        return velocity;
     }
 
     public String getName() {
@@ -67,22 +69,134 @@ class Car {
 }
 
 class Fortuner extends Car{
-    public Fortuner(String name, int cylinder) {
+    public Fortuner() {
         super("Fortuner", 6);
     }
 
     public int accelerate(int speed) {
-
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            if (speed < 5) {
+                velocity += 5;
+            } else {
+                velocity += (speed - 5);
+            }
+            System.out.println("The car " + getName() + " velocity was added by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
     }
 
     public int brake(int speed) {
-        
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            if (speed < 5) {
+                velocity -= speed;
+            } else {
+                velocity -= (speed - 5);
+            }
+            System.out.println("The car " + getName() + " velocity was reduced by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
+    }
+}
+
+class Ertiga extends Car {
+    public Ertiga() {
+        super("Ertiga", 3);
+    }
+
+    public int accelerate(int speed) {
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            velocity += speed;
+            System.out.println("The car " + getName() + " velocity was added by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
+    }
+
+    public int brake(int speed) {
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            velocity -= speed;
+            System.out.println("The car " + getName() + " velocity was reduced by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
+    }
+}
+
+class LandCruise extends Car {
+    public LandCruise() {
+        super("Land Cruiser", 8);
+    }
+
+    public int accelerate(int speed) {
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            if (speed < 10) {
+                velocity += speed;
+            } else {
+                velocity += (speed - 10);
+            }
+            System.out.println("The car " + getName() + " velocity was added by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
+    }
+
+    public int brake(int speed) {
+        int velocity = this.getVelocity();
+        if (speed < 0) {
+            System.out.println("The car " + getName() + " is moving constantly at " + velocity);
+        } else {
+            if (speed < 10) {
+                velocity -= speed;
+            } else {
+                velocity -= (speed - 10);
+            }
+            System.out.println("The car " + getName() + " velocity was reduced by " +
+                    speed + " and now become " + velocity);
+        }
+        return velocity;
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
+        for (int i=1; i<5; i++) {
+            Car car = randomCar();
+            System.out.println("Car #" + i +
+                    " : " + car.getName() + "\n" +
+                    car.accelerate(20) + "\n" +
+                    car.brake(15));
+        }
+    }
 
+    public static Car randomCar() {
+        int randomNumber =(int) (Math.random() * 3) + 1;
+        System.out.println("Car selection = ");
+        switch (randomNumber) {
+            case 1:
+                return new Fortuner();
+            case 2:
+                return new Ertiga();
+            case 3:
+                return new LandCruise();
+        }
+        return null;
     }
 }
